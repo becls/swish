becls/swish

commit 94f85fba79368dbcd1ba6acbe6b2f3780871d5d3

exit => raise, define-record => define-tuple, define-state-record => define-state-tuple

src/swish/app-io.ss
@@ -31,10 +31,11 @@
    web-path
    )
   (import
+   (chezscheme)
    (software-info)
    (swish erlang)
    (swish io)
-   (except (chezscheme) define-record exit))
+   )
 
   (define bin-dir (cd))
 

src/swish/erlang.ss
@@ -29,7 +29,7 @@
    complete-io
    console-event-handler
    dbg
-   define-record
+   define-tuple
    demonitor
    demonitor&flush
    erlang:now
@@ -50,7 +50,6 @@
    profile-me
    receive
    register
-   scheme-exit
    self
    send
    spawn
@@ -61,12 +60,10 @@
    whereis
    )
   (import
+   (chezscheme)
    (swish meta)
    (swish osi)
-   (rename (except (chezscheme) define-record remove)
-     (exit scheme-exit)
-     (raise exit)))
-
+   )
   ;; Procedures starting with @ must be called with interrupts disabled.
 
   (define-syntax on-exit
@@ -202,7 +199,7 @@
     (when (eq? p finalizer-process)
       (panic `#(finalizer-process-terminated ,reason)))
     (when (enqueued? p)
-      (remove p))
+      (remove-q p))
     (pcb-cont-set! p #f)
     (pcb-winders-set! p '())
     (pcb-exception-state-set! p reason)
@@ -255,7 +252,7 @@
            (lambda () e1 e2 ...))))]))
 
   (define (bad-arg who arg)
-    (exit `#(bad-arg ,who ,arg)))
+    (raise `#(bad-arg ,who ,arg)))
 
   (define (kill p reason)
     (unless (pcb? p)
@@ -348,12 +345,12 @@
 
   (define (@enqueue process queue precedence)
     (when (enqueued? process)
-      (remove process))
+      (remove-q process))
     (pcb-precedence-set! process precedence)
     (let find ([next queue])
       (let ([prev (q-prev next)])
         (if (or (eq? prev queue) (<= (pcb-precedence prev) precedence))
-            (insert process next)
+            (insert-q process next)
             (find prev)))))
 
   (define last-process-id 0)
@@ -465,7 +462,7 @@
   (define (@send p x)
     (let ([inbox (pcb-inbox p)])
       (when inbox
-        (insert (make-msg x) inbox)
+        (insert-q (make-msg x) inbox)
         (cond
          [(pcb-sleeping? p)
           (pcb-sleeping?-set! p #f)
@@ -489,14 +486,14 @@
      [(and (or (fixnum? timeout) (bignum? timeout)) (>= timeout 0))
       ($receive matcher src (+ (erlang:now) timeout) timeout-handler)]
      [(eq? timeout 'infinity) ($receive matcher src #f #f)]
-     [else (exit `#(timeout-value ,timeout ,src))]))
+     [else (raise `#(timeout-value ,timeout ,src))]))
 
   (define (receive-until matcher src time timeout-handler)
     (cond
      [(and (or (fixnum? time) (bignum? time)) (>= time 0))
       ($receive matcher src time timeout-handler)]
      [(eq? time 'infinity) ($receive matcher src #f #f)]
-     [else (exit `#(timeout-value ,time ,src))]))
+     [else (raise `#(timeout-value ,time ,src))]))
 
   (define ($receive matcher src waketime timeout-handler)
     (disable-interrupts)
@@ -524,7 +521,7 @@
           (cond
            [(matcher (msg-contents msg)) =>
             (lambda (run)
-              (remove msg)
+              (remove-q msg)
               (run))]
            [else
             (disable-interrupts)
@@ -534,7 +531,7 @@
 
   (define tick-nanoseconds 1000000) ;; 1 millisecond
 
-  (define (insert x next)
+  (define (insert-q x next)
     ;; No interrupts occur within this procedure because the record
     ;; functions get inlined.
     (let ([prev (q-prev next)])
@@ -543,7 +540,7 @@
       (q-next-set! x next)
       (q-prev-set! next x)))
 
-  (define (remove x)
+  (define (remove-q x)
     ;; No interrupts occur within this procedure because the record
     ;; functions get inlined.
     (let ([prev (q-prev x)] [next (q-next x)])
@@ -586,14 +583,14 @@
            (pcb-cont-set! self k)
            (cond
             [queue (@enqueue self queue precedence)]
-            [(enqueued? self) (remove self)]))
+            [(enqueued? self) (remove-q self)]))
 
          ;; context switch
          (pcb-sic-set! self prev-sic)
          (let ([p (q-next run-queue)])
            (when (eq? p run-queue)
              (panic 'run-queue-empty))
-           (set-self! (remove p)))
+           (set-self! (remove-q p)))
 
          ;; adjust system interrupt counter for the new process
          (let loop ([sic (pcb-sic self)])
@@ -674,11 +671,11 @@
       (bad-arg 'register p))
     (with-interrupts-disabled
      (cond
-      [(not (alive? p)) (exit `#(process-dead ,p))]
+      [(not (alive? p)) (raise `#(process-dead ,p))]
       [(pcb-name p) =>
-       (lambda (name) (exit `#(process-already-registered ,name)))]
+       (lambda (name) (raise `#(process-already-registered ,name)))]
       [(eq-hashtable-ref registrar name #f) =>
-       (lambda (pid) (exit `#(name-already-registered ,pid)))]
+       (lambda (pid) (raise `#(name-already-registered ,pid)))]
       [else
        (pcb-name-set! p name)
        (eq-hashtable-set! registrar name p)
@@ -821,7 +818,7 @@
       [(_) #f]))
 
   (define (bad-match v src)
-    (exit `#(bad-match ,v ,src)))
+    (raise `#(bad-match ,v ,src)))
 
   (define-syntax (match-one x)
     (define (bad-pattern x)
@@ -958,7 +955,7 @@
        (match-help (#3%vector-ref v i) pattern fail
          (match-vector v (fx+ i 1) rest fail body))]))
 
-  (define-syntax (define-record x)
+  (define-syntax (define-tuple x)
     (syntax-case x ()
       [(_ name field ...)
        (and (identifier? #'name)
@@ -988,7 +985,7 @@
                        (define tmp
                          (let ([val #,expr])
                            (unless (name is? val)
-                             (exit `#(bad-record name ,val ,#,(find-source x))))
+                             (raise `#(bad-record name ,val ,#,(find-source x))))
                            val))
                        #,@(map make-accessor (syntax->list field-names)))))
              (define (handle-copy x e bindings mode)
@@ -996,7 +993,7 @@
                    #,(case mode
                        [copy
                         #`(unless (name is? src)
-                            (exit `#(bad-record name ,src ,#,(find-source x))))]
+                            (raise `#(bad-record name ,src ,#,(find-source x))))]
                        [copy*
                         (handle-open x #'src #f (get-binding-names bindings))])
                    (vector 'name #,@(copy-record #'(field ...) 1 bindings))))
@@ -1093,7 +1090,7 @@
                 (syntax-datum-eq? #'fn #'field)
                 #`(lambda (x)
                     (unless (name is? x)
-                      (exit `#(bad-record name ,x ,#,(find-source x))))
+                      (raise `#(bad-record name ,x ,#,(find-source x))))
                     (#3%vector-ref x #,(find-index #'fn #'(field ...) 1)))]
                ...
                [(name no-check fn e)

src/swish/event-mgr.ms
@@ -21,17 +21,17 @@
 ;;; DEALINGS IN THE SOFTWARE.
 
 (import
+ (except (chezscheme) sleep)
  (swish erlang)
  (swish event-mgr)
  (swish mat)
  (swish string-utils)
  (swish testing)
- (except (chezscheme) define-record exit sleep)
  )
 
 (define (reported event)
   (receive
-   (after 1000 (exit `#(event-timeout ,event)))
+   (after 1000 (raise `#(event-timeout ,event)))
    [,@event 'ok]))
 
 (define (silence-log-handler)
@@ -92,15 +92,15 @@
   (let* ([caller self]
          [pid (spawn&link
                (lambda ()
-                 (event-mgr:add-handler exit)
+                 (event-mgr:add-handler raise)
                  (send caller 'ready)
-                 (receive (after 1000 (exit 'timeout)))))])
+                 (receive (after 1000 (raise 'timeout)))))])
     (receive
-     (after 1000 (exit 'timeout-waiting-to-start))
+     (after 1000 (raise 'timeout-waiting-to-start))
      [ready 'ok])
     (event-mgr:notify 'crash)
     (reported 'crash)
-    (receive (after 1000 (exit 'timeout-waiting))
+    (receive (after 1000 (raise 'timeout-waiting))
       [#(EXIT ,@pid crash) (void)]))
   'ok)
 

doc/scheme-coding-standard.tex
@@ -166,15 +166,15 @@ With \code{match}:
     [(,first . ,rest) (fold f (f first acc) rest)]))
 \end{alltt}
 
-\subsection* {Use match or match-let* with records}
+\subsection* {Use match or match-let* with tuples}
 
-When binding multiple fields from a record, use \code{match} or
+When binding multiple fields from a tuple, use \code{match} or
 \code{match-let*} to minimize type checking.
 
 Given:
 \antipar
 \begin{alltt}
-(define-record <point> x y z)
+(define-tuple <point> x y z)
 \end{alltt}
 
 Don't extract each field:
@@ -206,14 +206,14 @@ Don't do this:
 \begin{alltt}
 (if \var{condition}
     (success)
-    (exit 'boom!))
+    (raise 'boom!))
 \end{alltt}
 
 Do this:
 \antipar
 \begin{alltt}
 (unless \var{condition}
-  (exit 'boom!))
+  (raise 'boom!))
 (success)
 \end{alltt}
 
@@ -275,7 +275,7 @@ For example in \texttt{swish/gen-server.ss} we find the
 \begin{alltt}
 (library (swish gen-server)
   (export
-   define-state-record
+   define-state-tuple
    gen-server:call
    gen-server:cast
    gen-server:debug
@@ -284,10 +284,10 @@ For example in \texttt{swish/gen-server.ss} we find the
    gen-server:start&link
    )
   (import
+   (chezscheme)
    (swish erlang)
    (swish event-mgr-notify)
    (swish events)
-   (except (chezscheme) define-record exit)
    )
 \end{alltt}
 

doc/swish.sty
@@ -29,7 +29,7 @@
 \makedecl{procedure}
 \makedecl{property}
 \makedecl{syntax}
-\makedecl{record}
+\makedecl{tuple}
 \newcommand{\returns}{\makeresults{returns}}
 \newcommand{\expandsto}{\makeresults{expands to}}
 \newcommand{\hasvalue}{\makeresults{value}}
@@ -37,7 +37,7 @@
 \newenvironment{pubevent}[1]{\begin{event}\codeindex{#1}\end{event}\antipar
   \begin{argtbl}}
   {\end{argtbl}}
-\newenvironment{recorddef}[1]{\begin{record}\codeindex{#1}\end{record}\antipar
+\newenvironment{tupledef}[1]{\begin{tuple}\codeindex{#1}\end{tuple}\antipar
   \begin{argtbl}}{\end{argtbl}}
 \newcommand{\concern}[1]{\textbf{Concern:} #1}
 \newcommand{\mitigation}{\textbf{Mitigation: }}

doc/swish/application.tex
@@ -77,7 +77,7 @@ to determine if the application shut down normally.
 \returns{} never
 
 The \code{exit-process} procedure flushes the console output port,
-ignoring any exceptions, and then calls \code{(ExitProcess
+ignoring any exceptions, and then calls \code{(osi\_exit
   \var{exit-code})}.
 
 \section {Programming Interface}

doc/swish/db.tex
@@ -86,7 +86,7 @@ interact with SQLite. To prevent memory leaks, raw database handles
 are wrapped in a Scheme record and registered with a guardian.
 
 \paragraph* {state}\index{db!state}
-\code{(define-state-record <db-state> filename db cache queue worker)}
+\code{(define-state-tuple <db-state> filename db cache queue worker)}
 \begin{itemize}
 \item \code{filename} is the database specified when the server was
   started.
@@ -176,7 +176,7 @@ processes the following message:
 
 \antipar\begin{itemize}
 
-\item \code{\#(log \var{sql} \var{bindings})}: Add this record to
+\item \code{\#(log \var{sql} \var{bindings})}: Add this tuple to
   the queue. Process the queue.
 
 \end{itemize}
@@ -329,7 +329,7 @@ this is a list of rows as returned by a \code{SELECT} query.
 \expandsto{} \antipar\begin{alltt}
 (match (db:transaction \var{db} (lambda () \var{body} \etc))
   [#(ok ,result) result]
-  [#(error ,reason) (exit reason)])
+  [#(error ,reason) (raise reason)])
 \end{alltt}
 
 The \code{transaction} macro runs the body in a transaction and

doc/swish/erlang.tex
@@ -31,7 +31,7 @@ language~\cite{armstrong-thesis,programming-erlang}.\footnote{Tuples,
   denoted by \{$e_1$, \ldots, $e_n$\} in Erlang, are implemented as
   vectors: \code{\#(\var{e$_1$}~\etc~\var{e$_n$})}. Similarly
   records, defined as syntactic sugar over tuples in Erlang, are
-  implemented as syntactic sugar over vectors.}  Record and pattern
+  implemented as syntactic sugar over vectors.}  Tuple and pattern
 matching macros provide succinct ways of composing and decomposing
 data structures.
 
@@ -389,20 +389,6 @@ registered process names from the registrar.
 \subsection {Process Termination, Links, and Monitors}
 
 % ----------------------------------------------------------------------------
-\defineentry{exit}
-\begin{procedure}
-  \code{(exit \var{reason})}
-\end{procedure}
-\returns{} never
-
-The \code{exit} procedure is the system \code{raise} procedure,
-which raises an exception of \var{reason}. If the calling process does
-not catch the exception, the process will exit with \var{reason}.
-
-The system \code{exit} procedure is exported as
-\code{scheme-exit}\index{scheme-exit@\code{scheme-exit}}.
-
-% ----------------------------------------------------------------------------
 \defineentry{catch}
 \begin{syntax}
   \code{(catch \var{e1} \var{e2} \etc{})}
@@ -552,7 +538,7 @@ provides a concise and expressive way to match data structures and
 bind variables to parts. The \code{receive}, \code{match}, and
 \code{match-let*} macros use this pattern language. The
 implementation uses two structurally recursive passes over the
-pattern. The first pass checks the pattern syntax including record
+pattern. The first pass checks the pattern syntax including tuple
 types and field names and accumulates the list of pattern
 variables. This list enables it to check for duplicate pattern
 variables.  The second pass emits code that matches the input against
@@ -586,7 +572,7 @@ the pattern left to right.
   matches \var{pattern} and binds a fresh \var{variable} to it \\
 
   \code{`(\var{type} \set{,\var{field}\alt{}[\var{field} \var{pattern}]} \etc{})} &
-  an instance of the record \var{type}, each \var{field} of which is
+  an instance of the tuple \var{type}, each \var{field} of which is
   bound to fresh variable \var{field} or matches the corresponding
   \var{pattern} \\
 
@@ -910,58 +896,58 @@ profiling is turned off, \code{(profile-me)} expands to
 \code{(void)}, and the system optimizer eliminates it.
 
 % ----------------------------------------------------------------------------
-\subsection {Records}
+\subsection {Tuples}
 
-For users of the concurrency model, a \emph{record}\index{record} is a
+For users of the concurrency model, a \emph{tuple}\index{tuple} is a
 container of named, immutable fields implemented as a vector whose
-first element is the record name and remaining elements are the
-fields. Each record definition is a macro that provides all record
+first element is the tuple name and remaining elements are the
+fields. Each tuple definition is a macro that provides all tuple
 operations using field names only, not field indices. The macro makes
-it easy to copy a record without having to specify the fields that
+it easy to copy a tuple without having to specify the fields that
 don't change.  We decided not to use the Scheme record facility
 because it does not provide name-based constructors, copy operators,
 or convenient serialization.
 
 % ----------------------------------------------------------------------------
-\index{record!define-record@\code{define-record}}
-\defineentry{define-record}
+\index{tuple!define-tuple@\code{define-tuple}}
+\defineentry{define-tuple}
 \begin{syntax}
-  \code{(define-record \var{name} \var{field} \etc{})}
+  \code{(define-tuple \var{name} \var{field} \etc{})}
 \end{syntax}
 \expandsto{} a macro definition of \var{name} described below
 
-The \code{define-record} macro defines a macro for creating,
-copying, identifying, and accessing record type \var{name}. \var{name}
+The \code{define-tuple} macro defines a macro for creating,
+copying, identifying, and accessing tuple type \var{name}. \var{name}
 and \var{field} \etc{} must be identifiers. No two field names can be
 the same. The following field names are reserved: \code{make},
 \code{copy}, \code{copy*}, and \code{is?}.
 
 % ----------------------------------------------------------------------------
-\index{record!make@\code{make}}
+\index{tuple!make@\code{make}}
 \begin{syntax}
   \code{(\var{name} make [\var{field} \var{value}] \etc{})}
 \end{syntax}
-\returns{} a new instance of record type \var{name} with \var{field} =
+\returns{} a new instance of tuple type \var{name} with \var{field} =
 \var{value} \etc{}
 
-The \code{make} form creates a new instance of the record type
+The \code{make} form creates a new instance of the tuple type
 \var{name}. \var{field} bindings may appear in any order. All fields
-from the record definition must be specified.
+from the tuple definition must be specified.
 
 % ----------------------------------------------------------------------------
-\index{record!field accessor}
+\index{tuple!field accessor}
 \begin{syntax}
   \code{(\var{name} \var{field} \var{instance})}
 \end{syntax}
 \returns{} \var{instance}.\var{field}
 
 The field accessor form retrieves the value of the specified
-\var{field} of \var{instance}. If $r$ = \var{instance} is not a record
-of type \var{name}, exception \code{\#(bad-record \var{name} $r$
+\var{field} of \var{instance}. If $r$ = \var{instance} is not a tuple
+of type \var{name}, exception \code{\#(bad-tuple \var{name} $r$
   \var{src})} is raised, where \var{src} is the source location of the
 field accessor form if available.
 
-\index{record!field accessor}
+\index{tuple!field accessor}
 \begin{syntax}
   \code{(\var{name} \var{field})}
 \end{syntax}
@@ -970,7 +956,7 @@ field accessor form if available.
 The \code{(\var{name} \var{field})} form expands to \code{(lambda (instance) (\var{name} \var{field} instance))}.
 
 % ----------------------------------------------------------------------------
-\index{record!open@\code{open}}
+\index{tuple!open@\code{open}}
 \begin{syntax}
   \code{(\var{name} open \var{instance} \opt{\var{prefix}} (\var{field} \etc{}))}
 \end{syntax}
@@ -979,11 +965,11 @@ The \code{(\var{name} \var{field})} form expands to \code{(lambda (instance) (\v
 The \code{open} form defines identifier syntax for each specified \var{field}
 so that a reference to \var{field} expands to \code{(\var{name} \var{field} $r$)}
 where $r$ is the value of \var{instance}.
-If $r$ is not a record of type \var{name}, exception
-\code{\#(bad-record \var{name} $r$ \var{src})} is raised, where
+If $r$ is not a tuple of type \var{name}, exception
+\code{\#(bad-tuple \var{name} $r$ \var{src})} is raised, where
 \var{src} is the source location of the \code{open} form if avalable.
 The \code{open} form is equivalent to the following, except that it
-checks the record type only once:
+checks the tuple type only once:
 \antipar\codebegin
 (begin
   (define instance \var{instance})
@@ -996,35 +982,35 @@ a definition for \code{\var{prefix}\var{field}} rather than \var{field} for
 each \var{field} specified.
 
 % ----------------------------------------------------------------------------
-\index{record!copy@\code{copy}}
+\index{tuple!copy@\code{copy}}
 \begin{syntax}
   \code{(\var{name} copy \var{instance} [\var{field} \var{value}] \etc{})}
 \end{syntax}
-\returns{} a new instance of record type \var{name} with \var{field} =
+\returns{} a new instance of tuple type \var{name} with \var{field} =
 \var{value} \etc{} and remaining fields copied from \var{instance}
 
 The \code{copy} form creates a copy of \var{instance} except that
 each specified \var{field} is set to the associated \var{value}. If
-$r$ = \var{instance} is not a record of type \var{name}, exception
-\code{\#(bad-record \var{name} $r$ \var{src})} is raised, where
+$r$ = \var{instance} is not a tuple of type \var{name}, exception
+\code{\#(bad-tuple \var{name} $r$ \var{src})} is raised, where
 \var{src} is the source location of the \code{copy} form if
 avalable.  \var{field} bindings may appear in any order.
 
 % ----------------------------------------------------------------------------
-\index{record!copy*@\code{copy*}}
+\index{tuple!copy*@\code{copy*}}
 \begin{syntax}
   \code{(\var{name} copy* \var{instance} [\var{field} \var{value}] \etc{})}
 \end{syntax}
-\returns{} a new instance of record type \var{name} with \var{field} =
+\returns{} a new instance of tuple type \var{name} with \var{field} =
 \var{value} \etc{} and remaining fields copied from \var{instance}
 
 The \code{copy*} form is like \code{copy} except that, within the \var{value} expressions,
 each specified \var{field} is bound to an identifier macro that returns the value of \var{instance.field}.
-If $r$ = \var{instance} is not a record of type \var{name}, exception
-\code{\#(bad-record \var{name} $r$ \var{src})} is raised, where
+If $r$ = \var{instance} is not a tuple of type \var{name}, exception
+\code{\#(bad-tuple \var{name} $r$ \var{src})} is raised, where
 \var{src} is the source location of the \code{copy*} form if avalable.
 The \code{copy*} form is equivalent to the following, except that it
-checks the record type only once:
+checks the tuple type only once:
 \antipar\codebegin
 (let ([instance \var{instance}])
   (\var{name} open instance (\var{field} \etc{}))
@@ -1032,14 +1018,14 @@ checks the record type only once:
 \codeend\antipar
 
 % ----------------------------------------------------------------------------
-\index{record!is?@\code{is?}}
+\index{tuple!is?@\code{is?}}
 \begin{syntax}
   \code{(\var{name} is? \var{x})}
 \end{syntax}
 \returns{} a boolean
 
 The \code{is?} form determines whether or not the datum \var{x} is
-an instance of record type \var{name}.
+an instance of tuple type \var{name}.
 
 % ----------------------------------------------------------------------------
 \subsection {I/O}

commit a8e8e1ad6c1a457229ba5dd4bf7a6a423e15e92d

more exit -> raise fallout from 94f85fba79368dbcd1ba6acbe6b2f3780871d5d3

src/swish/app-io.ss
@@ -54,6 +54,6 @@
            (tmp-dir (path-combine (data-dir) "tmp"))
            (web-dir (path-combine base "web"))
            base]
-          [#(EXIT ,reason) (exit reason)]
+          [#(EXIT ,reason) (raise reason)]
           [#f (errorf 'base-dir "no directory ~s" base)]))))
   )

src/swish/erlang.ss
@@ -34,7 +34,6 @@
    demonitor&flush
    dump-stack
    erlang:now
-   exit
    get-registered
    kill
    link

src/swish/event-mgr.ms
@@ -156,7 +156,7 @@
     (let ([lines (split (get) #\newline)])
       (define (assert-prefix! pattern)
         (match lines
-          [() (exit `#(pattern-not-found ,pattern))]
+          [() (raise `#(pattern-not-found ,pattern))]
           [(,line . ,rest)
            (set! lines rest)
            (unless (starts-with? line pattern)