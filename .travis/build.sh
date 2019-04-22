74174332a8cc417a9e7b30378f3378edf320cf59
Bob Burger <burgerrg@gmail.com>
2019-04-22 11:20:16 -0400

    update build script so that times for each part are displayed

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 set -e -o pipefail
-CSV=csv9.5.2
-echo 'travis_fold:start:ChezScheme'
-echo Installing Chez Scheme...
-echo Downloading $CSV
-curl -Ls https://github.com/cisco/ChezScheme/releases/download/v9.5.2/csv9.5.2.tar.gz | tar -xzf -
-echo Building $CSV
-(cd $CSV; ./configure -m=$TARGET_MACHINE; make)
-echo 'travis_fold:end:ChezScheme'
-CSROOT=$PWD/$CSV/$TARGET_MACHINE
+CSROOT=$PWD/chez/$TARGET_MACHINE
 export SCHEMEHEAPDIRS=$CSROOT/boot/$TARGET_MACHINE
 echo 'travis_fold:start:Swish'
 echo Building Swish...
 ./configure --scheme=$CSROOT/bin/$TARGET_MACHINE/scheme
 make
 echo 'travis_fold:end:Swish'
-echo Testing Swish...
-make test
---
+++ .travis/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -o pipefail
+CSROOT=$PWD/chez/$TARGET_MACHINE
+export SCHEMEHEAPDIRS=$CSROOT/boot/$TARGET_MACHINE
+echo Testing Swish...
+make test || {
+    rc=$?
+    echo 'travis_fold:start:failures'
+    echo Mat output
+    cat src/swish/*.mo || true
+    echo 'travis_fold:end:failures'
+    exit $rc
+}
